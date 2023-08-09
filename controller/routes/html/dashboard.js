9f6029c09ac8e5fda587a3a777bda4da0b9e98a8
cesarsiguencia <cesar.siguencia@gmail.com>
2023-08-08 20:48:14 -0400

    after numerious attempts, OG fetching is finally asychronous and displaying on html

--- controller/routes/html/dashboard.js
+++ controller/routes/html/dashboard.js
@@ -4,6 +4,8 @@ const { Post, Comment, User } = require('../../../models')
 
 const { getLinkPreview, getPreviewFromContent } = require("link-preview-js")
 
+
+
 router.get('/', (req, res) => {
     Post.findAll({
         include: [
@@ -12,9 +14,9 @@ router.get('/', (req, res) => {
                 attributes: ['username', 'rsvp']
             },
             {
-                model: Comment, 
-                attributes: ['id','comment_text', 'user_id', 'post_id'],
-                include:{
+                model: Comment,
+                attributes: ['id', 'comment_text', 'user_id', 'post_id'],
+                include: {
                     model: User,
                     attributes: ['username', 'id']
                 }
@@ -25,64 +27,51 @@ router.get('/', (req, res) => {
         ]
     })
         .then(postsFromDb => {
-            const posts = postsFromDb.map(post => 
-                
+            const uneditedPosts = postsFromDb.map(post =>
                 post.get({
-                plain: true
+                    plain: true
                 })
             );
-
-            
-
-            posts.forEach(fetchedPost => {
-
-                if(fetchedPost.post_link){
-                    var returned = fetchOG(fetchedPost.post_link)
-                    console.log(returned)
-                    if(!returned){
-                        console.log('nothing was returned')
-                    } else {
-                        fetchedPost.openGraphMaterial = returned
-                    }
-                    
-                }
-                
-            })
-            console.log(posts)
-            res.render('dashboard', {
-                posts,
-                loggedUserId: req.session.user_id,
-                loggedIn: req.session.loggedIn,
-                
-            })
+            async function renderingComplete() {
+                const posts = await creatingOGPosts(uneditedPosts)
+                setTimeout(()=>{
+                    res.render('dashboard', {
+                        posts,
+                        loggedUserId: req.session.user_id,
+                        loggedIn: req.session.loggedIn,
+                    })
+                },100)
+            }
+            renderingComplete()
         })
         .catch(err => {
             res.status(500).json(err)
         })
 });
 
-async function fetchOG(website){
-    // // const url = document.querySelector("a[post-link-id='2']")
-    // console.log(url)
 
-    console.log(website, "we should have a website here")
+async function creatingOGPosts(newArray) {
+    var fetchingOGs = () => {
 
-    // var link = String(website)
-    // console.log(link, 'this is a string')
+        return new Promise(resolve => {
+            newArray.forEach(fetchedPost => {
 
-    var urls = await getLinkPreview("https://www.dinosaurbarbque.com").then((data)=>{
-        return data
-    }).catch(err => {
-        console.log("no OG available for picked website, return undefined")
-    })
-
-    // if(!urls){
-    //     urls = website
-    // }
-    console.log(urls, 'this is what we are waiting')
-    return urls
-
-    
+                if (fetchedPost.post_link) {
+                    getLinkPreview(fetchedPost.post_link).then((data) => {
+                        fetchedPost.openGraphMaterial = data
+                        return new Promise(resolve => {
+                            resolve(data)
+                        })
+                    }).catch(err => {
+                        return undefined
+                    })
+                }
+            })
+            resolve(newArray)
+        })
+    }
+    const newEditedPosts = await fetchingOGs()
+    return newEditedPosts
 }
 
 module.exports = router
