79696e7fac75507688d1b0e14b41273002503ecb
cesarsiguencia <cesar.siguencia@gmail.com>
2023-08-09 12:51:50 -0400

    adding heroku compatability

--- controller/config/connections.js
+++ controller/config/connections.js
@@ -1,16 +1,31 @@
-const Sequelize = require('sequelize')
+// const Sequelize = require('sequelize')
 
-require('dotenv').config()
+// require('dotenv').config()
 
-const sequelize = new Sequelize(
-    {
-        username: process.env.MYSQLUSER,
-        password: process.env.MYSQLPASSWORD,
-        database: process.env.MYSQLDATABASE,
-        dialect: 'mysql',
-        port: process.env.MYSQLPORT,
-        host: process.env.MYSQLHOST
-    }
-)
+// const sequelize = new Sequelize(
+//     {
+//         username: process.env.MYSQLUSER,
+//         password: process.env.MYSQLPASSWORD,
+//         database: process.env.MYSQLDATABASE,
+//         dialect: 'mysql',
+//         port: process.env.MYSQLPORT,
+//         host: process.env.MYSQLHOST
+//     }
+// )
+// module.exports = sequelize;
+
+const Sequelize = require('sequelize');
+require('dotenv').config();
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.MYSQLDATABSE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+} 
+
 
 module.exports = sequelize
