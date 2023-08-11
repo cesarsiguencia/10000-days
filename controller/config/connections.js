064778bc33c232ba04384404ce68ac0649dc0868
cesarsiguencia <cesar.siguencia@gmail.com>
2023-08-11 11:34:13 -0400

    added new localhost credentials

--- controller/config/connections.js
+++ controller/config/connections.js
@@ -20,11 +20,16 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(process.env.MYSQLDATABSE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
+  sequelize = new Sequelize(
+    {
+        username: process.env.MYSQLUSER,
+        password: process.env.MYSQLPASSWORD,
+        database: process.env.MYSQLDATABASE,
+        dialect: 'mysql',
+        port: process.env.MYSQLPORT,
+        host: process.env.MYSQLHOST
+    }
+)
 } 
 
 
